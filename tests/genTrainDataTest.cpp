818a210c76f1f7720a0e80b8eee6b0bb0910f98b
Lukas Chenghan Huan <lukas@huan.eu>
2023-07-30 13:43:48 +0200

    fix test

--- tests/genTrainDataTest.cpp
+++ tests/genTrainDataTest.cpp
@@ -6,18 +6,22 @@ TEST_CASE("repeatDataLabels") {
     SECTION("Test that dimension are correct") {
         std::vector<int> importance = {1, 2, 3};
         // Create a matrix with 3 rows and 4 columns
-        arma::mat data(4, 3, arma::fill::zeros);
+        arma::mat data(3, 4, arma::fill::zeros);
         data.at(0, 0) = 0.;
-        data.at(1, 0) = 1.;
-        data.at(2, 0) = 1.;
-        data.at(3, 0) = 2.;
+        data.at(0, 1) = 1.;
+        data.at(0, 2) = 1.;
+        data.at(0, 3) = 2.;
         arma::Row<size_t> labels = ("0 1 0 1");
         
         std::pair<arma::mat, arma::Row<size_t>> result = repeatDataLabels(data, labels, importance);
         //  we should have 1 + 2 * 2 + 3 = 8 rows
-        REQUIRE(result.first.n_rows == 8);
-        REQUIRE(result.second.n_cols == 8);
-        REQUIRE(result.first.n_cols == 3);
+        std::cout << result.first.n_rows << std::endl;
+        std::cout << result.second.n_rows << std::endl;
+        std::cout << result.first.n_cols << std::endl;
+        std::cout << result.second.n_cols << std::endl;
+        REQUIRE(result.first.n_rows == 2);
+        REQUIRE(result.first.n_cols == 8);
         REQUIRE(result.second.n_rows == 1);
+        REQUIRE(result.second.n_cols == 8);
     }
 }
