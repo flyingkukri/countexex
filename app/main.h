ad5f494f75dde87b6f9967ab461af89262f400cb
TeWas <theresa.wasserer@gmail.com>
2023-08-12 14:55:20 +0200

    :children_crossing: Add permissive scheduler computation option

--- app/main.h
+++ app/main.h
@@ -7,31 +7,70 @@
 #include <cstddef>
 #include <string>
 #include <storm/models/sparse/Mdp.h>
+#include "genTrainData.h"
 
 
 typedef storm::models::sparse::Mdp<double> Mdp;
 
+/*! 
+*  This struct contains configuration information for the importance and safety property calculation
+*/
 typedef struct config{
+        /* We will not consider importance values being smaller than delta*c */
         double delta;
+        /* c represents the number of simulation runs*/
         int c;
+        /* l represents the maximal number of simulation steps we perform in each simulation run*/
         int l;
+        /* Precision for the safety property bound*/
         int prec;
 } config;
 
+/*! 
+*  This struct contains information about the decision tree tuning parameterss
+*/
 struct DtConfig{
         double minimumGainSplit; 
         size_t minimumLeafSize;
         size_t maximumDepth;
 };
 
+/*! 
+*  This struct contains the command line options
+*/
+struct Options{
+        /* Path to the model file.*/
+        std::string pathToModel;
+        /* Specifies whether Pmax or Pmin will be checked. If set to true, Pmax will be checked.*/
+        bool propertyMax;
+        /* If set to true, prints additional output during the program execution*/
+        bool verbose;
+        /* Decides on permissive strategy computation: if set to true SMT is used, else MILP*/
+        bool optimizerSMT;
+        /* Struct containing configuration information for the importance and safety property calculation*/
+        config conf;
+        /* Struct containing information about the decision tree tuning parameters*/
+        DtConfig dtConfig;
+};
+
 /*!
 *  Helper to initialize the CL options
 * @param argc: arguments from main function
 * @param argv: arguments from main function
-* For the other parameters, see pipeline.
+* @param clOptions: struct containing the command line options
 * @return: 1 or 0 indicating whether the setup was successful or not
 */
-int initializeOptions(int argc, char *argv[], std::string& model, bool& max, bool& verbose, config& conf, DtConfig& dtConfig);
+int initializeOptions(int argc, char *argv[], Options& clOptions);
+
+/*!
+*  Helper to compute the importance values and create the value maps
+* @param permissiveScheduler: permissive scheduler, either computed via SMT or MILP
+* @param clOptions: struct containing the command line options
+* @param label: target states label
+* @param safetyMdp: MDP, from which the valueMap is constructed
+* @return: a pair containing the valueMap of the safety mdp and the submdp, which results from applying the permissive scheduler 
+*/
+std::pair<ValueMap, ValueMap> createValueMapHelper(boost::optional<storm::ps::SubMDPPermissiveScheduler<>>& permissiveScheduler, Options& clOptions, MdpInfo& mdpInfo, std::string& label, std::shared_ptr<storm::models::sparse::Mdp<double>>& safetyMdp);
 
 /*!
  * Pipeline, performing the following steps:
@@ -42,10 +81,6 @@ int initializeOptions(int argc, char *argv[], std::string& model, bool& max, boo
  * 5. Calculate importance: Simulate c runs on MDP under strategy σ
  * 6. Create training data and train decision tree 
  * 7. Visualize the strategy as decision tree stored in DOT file 
- * @param path_to_model: Path to the model file
- * @param max: Specifies whether Pmax or Pmin will be checked. If set to true, Pmax will be checked.
- * @param conf: Struct containing configuration information for the importance and safety property calculation
- * @param dtConfig: Struct containing information about the decision tree tuning parameters
- * @param verbose: If set to true, prints additional output during the program execution
+ * @param clOptions: struct containing the command line options
  */
-void pipeline(std::string const& path_to_model, bool max, config  const& conf, DtConfig& dtConfig, bool verbose);
+void pipeline(Options& clOptions);
